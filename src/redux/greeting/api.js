c92ae1186f5695e0803d5c67420738b338e71aa6
usbbush <jeanbienvenusb@gmail.com>
2022-11-30 17:51:06 +0200

    Update api link address

--- src/redux/greeting/api.js
+++ src/redux/greeting/api.js
@@ -1,4 +1,4 @@
-const url = 'http://127.0.0.1:3000/api/greetings';
+const url = 'http://localhost:3000/api/greetings';
 const randomGreeting = async () => {
   let result = [];
   await fetch(url).then((res) => res.json()).then((data) => {
